4ed62155bd4e5d4b740055bcb3d50df905e51f26
YohanLas17 <118545694+YohanLas17@users.noreply.github.com>
2025-07-27 10:02:30 +0300

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,13 @@
+import os
+import setuptools
+
+
+long_description = ""
+
+setuptools.setup(
+    name = "DocMLLM",
+    version = "1.0.0",
+    author = "JF",
+    description = "",
+    packages = setuptools.find_packages(),
+)
